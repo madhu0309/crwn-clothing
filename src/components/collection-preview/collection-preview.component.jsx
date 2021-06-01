3c6f92cba4603728e8dd50bc5601e6bf44466e2e
Madhu Gunneboina <madhu@codeft.io>
2021-06-01 09:17:20 +0530

    updating our app with a shop page, collection preview component and collection preview item

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+import './collection-preview.styles.scss';
+
+// the computer will run slow on re-render if the array if huge
+const CollectionPreview = ({ title, items }) => (
+  
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {
+        items
+        .filter((item, id) => id < 4)
+        .map(({id, ...otherItemProps}) => (
+          <CollectionItem key={id} {...otherItemProps}/>
+        ))
+      }
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
